2a3ffa15b2ec4a3a13baf813c253f4f7610458d5
Stefan <stefan.opariuc@tss-yonder.com>
2018-03-25 10:01:02 +0300

    fixed

--- src/main/java/com/fiipractic/health/entity/repository/DoctorRepository.java
+++ src/main/java/com/fiipractic/health/entity/repository/DoctorRepository.java
@@ -3,8 +3,6 @@ package com.fiipractic.health.entity.repository;
 
 import com.fiipractic.health.entity.model.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
